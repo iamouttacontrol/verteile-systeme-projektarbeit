b64768f635b05d7e9af76c1430d63785f7f67941
Nils <no.rudnik@googlemail.com>
2024-02-06 08:44:32 +0100

    Website Alpha V 0.5 Es wird ein JSON String erzeugt.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var nickname;
+var chosenlanguage = "de";
+
 function buttonSendAction(){
     const textFenster = document.getElementById("chatTextArea");
     const eingabeFester = document.getElementById("chatTextEingabe");
@@ -5,4 +8,26 @@ function buttonSendAction(){
     var eingabeText = eingabeFester.value;
     textFenster.innerHTML = eingabeText;
     eingabeFester.value = "";
+
+    //JSON String absenden
+    const chatnachricht = {
+        name: nickname,
+        message: eingabeText,
+        language: chosenlanguage
+    }
+
+    const messageAsJsonString = JSON.stringify(chatnachricht);
+
+    textFenster.innerHTML = messageAsJsonString;
+
+}
+
+function buttonNicknameSave(){
+    const nicknameEingabe = document.getElementById("nicknameArea");
+    nickname = nicknameEingabe.value;
+    nicknameEingabe.readOnly = true;
+}
+
+function dropdownLanguageChange(){
+    
 }
