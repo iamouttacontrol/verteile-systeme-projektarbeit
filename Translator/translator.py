1c1c7a6b2ef17fc2c15cb279ef7f3786ec78d3f6
PhilipKlinger <philip.klinger28@icloud.com>
2024-02-06 14:56:01 +0100

    Translator V2

--- Translator/translator.py
+++ Translator/translator.py
@@ -1,22 +1,27 @@
 import json
 import os
 from google.cloud import translate_v2 as translate
+import html
 
 def translate_text(json):
     credentials_path = "credentials.json"
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] =credentials_path
     translate_client = translate.Client()
     message = json["message"]
-    print(message)
     target = json["language"]
-    print(target)
     if isinstance(message, bytes):
         message = message.decode("utf-8")
     result = translate_client.translate(message, target_language=target)
-    print(result)
+    result["translatedText"] = html.unescape(result["translatedText"])
     return result
 
-with open("tests/test1.json") as test:
-    inhalt = json.load(test)
+def translateAndConvert(json):
+    response = translate_text(json)
+    standard_format = {"name" : json["name"], "message" : response["translatedText"], "language" : json["language"], "timestamp" : json["timestamp"]}
+    return standard_format
 
-translate_text(inhalt)
+with open("tests/test.json") as test:
+    json = json.load(test)
+
+
+print(translateAndConvert(json))
