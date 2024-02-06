ef4fbe9761e04ca7a3bf1af9b737323c356ca083
HexFlash <asdasdas>
2024-02-06 16:34:35 +0100

    updated client and server

--- backend_code/client.py
+++ backend_code/client.py
@@ -1,26 +1,40 @@
 from twisted.internet import reactor, protocol
+from twisted.internet.protocol import ReconnectingClientFactory 
 import json
 
-class Client(protocol.Protocol):
+class Client(protocol.Protocol):    
+    def __init__(self, lang):
+        self.lang = lang
+    
     def connectionMade(self):
-        data = {"message": "Hello, server!"}
+        data = {"message": "Hello, server!", "lang":self.lang}
         self.transport.write(json.dumps(data).encode())
 
     def dataReceived(self, data):
         response = json.loads(data.decode())
         print("Response from server:", response)
 
-class ClientFactory(protocol.ClientFactory):
+class ClientFactory(protocol.ReconnectingClientFactory):
+    def __init__(self):
+        self.lang = "de"
+        
+    def startedConnecting(self, connector):
+        print('Started to connect.')
+        
     def buildProtocol(self, addr):
-        return Client()
+        print('Connected.')
+        print('Resetting reconnection delay')
+        self.resetDelay()
+        return Client(self.lang)
 
     def clientConnectionFailed(self, connector, reason):
         print("Connection failed:", reason)
-        reactor.stop()
+        ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
 
     def clientConnectionLost(self, connector, reason):
         print("Connection lost:", reason)
-        reactor.stop()
+        ReconnectingClientFactory.clientConnectionLost(self, connector, reason)
+        
 
 if __name__ == '__main__':
     reactor.connectTCP('localhost', 8000, ClientFactory())
--- backend_code/server.py
+++ backend_code/server.py
@@ -4,9 +4,13 @@ import json
 class Server(protocol.Protocol):
     def __init__(self, factory):
         self.factory = factory
+        print("SERVER CREATED")
         
     def connectionMade(self):
         self.factory.clients.add(self)
+        print("Client connected:", self.transport.getPeer())
+        print(len(self.factory.clients))
+        self.lang = "en"
         
     def connectionLost(self, reason):
         self.factory.clients.remove(self)
@@ -15,10 +19,13 @@ class Server(protocol.Protocol):
         received_data = json.loads(data.decode())
 
         print(received_data)
+        
+        self.lang = received_data["lang"]
 
         response_data = {"response": "Received data successfully"}
         
         for c in self.factory.clients:
+            print(c.lang)
             c.transport.write(json.dumps(response_data).encode())
 
 class ServerFactory(protocol.Factory):
@@ -29,6 +36,5 @@ class ServerFactory(protocol.Factory):
         return Server(self)
 
 if __name__ == '__main__':
-    # Server starten
     endpoints.serverFromString(reactor, "tcp:8000").listen(ServerFactory())
     reactor.run()
